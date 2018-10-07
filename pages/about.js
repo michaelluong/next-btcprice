baadd0cc905dbac0299c1b7704f27c776904cc8d
michaelluong <mluong517@gmail.com>
2018-10-06 20:19:22 -0700

    First commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+const Index = () => (
+    <Layout>
+        <div>
+            <h1>About BTCPrice</h1>
+            <p>Application to view Bitcoin prices</p>
+        </div>
+    </Layout>
+);
+
+export default Index;
